95d1049c39e37e02d63b553fda443835e5cdb668
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-07 00:30:26 +0100

    Case converter with list comprehension in Python

--- case_converter.py
+++ case_converter.py
@@ -1,4 +1,6 @@
 def convert_to_snake_case(pascal_or_camel_cased_string):
+    pass
+    '''
     snake_cased_char_list = []
     for char in pascal_or_camel_cased_string:
         if char.isupper():
@@ -8,4 +10,11 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
             snake_cased_char_list.append(char)
     snake_cased_string = ''.join(snake_cased_char_list)
     clean_snake_cased_string = snake_cased_string.strip('_')
-    print(clean_snake_cased_string)
+
+    return clean_snake_cased_string
+    '''
+def main():
+    print(convert_to_snake_case('aLongAndComplexString'))
+
+if __name__ == '__main__':
+    main()
