7763eb88a0cd0ec21d97f5a055d73b739af9247e
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-09 15:05:12 +0100

    Password Generator with Regular Expressions in Python

--- password_generator.py
+++ password_generator.py
@@ -3,7 +3,8 @@ import secrets
 import string
 
 
-def generate_password(length, nums, special_chars, uppercase, lowercase):
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -17,25 +18,24 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-       
+        
         constraints = [
             (nums, r'\d'),
-            (lowercase, r'[a-z]'),
-            (uppercase, r'[A-Z]'),            
-            (special_chars, fr'[{symbols}]')            
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
         ]
 
-        # Check constraints
-        count = 0
+        # Check constraints        
         if all(
-            [
-                constraint <= len(re.findall(pattern, password))
-                for constraint, pattern in constraints
-            ]
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
         ):
             break
     
     return password
+    
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
 
-# new_password = generate_password(8)
-# print(new_password)
