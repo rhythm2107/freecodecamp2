e707579b3cbe7646031f6ab64aa496da2cde06de
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-10 00:04:22 +0100

    Algorithm Design with Shortest Path Algorithm

--- algorithm_design.py
+++ algorithm_design.py
@@ -5,5 +5,12 @@ my_graph = {
     'D': [('A', 1), ('C', 7)]
 }
 
-def shortest_path():
-    pass
+def shortest_path(graph, start):
+    unvisited = []
+    distances = {}
+    for node in graph:
+        unvisited.append(node)
+        if node == start:
+            distances[node] = 0
+        else:
+            distances[node] = float('inf')
