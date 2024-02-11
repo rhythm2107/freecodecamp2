7b3db463a911b6426029415166080724403dce0b
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-11 15:21:35 +0100

    Tower of Hanoi with Recursion Python

---
+++ tower_of_hanoi.py
@@ -0,0 +1,12 @@
+NUMBER_OF_DISKS = 3
+number_of_moves = 2**NUMBER_OF_DISKS - 1
+rods = {
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
+    'B': [],
+    'C': []
+}
+
+def move(n, source, auxiliary, target):
+    print(rods)
+ #Initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, 'A', 'B', 'C')
