edaf21cdcca909bb1a6121cdb52c8636499cafc3
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-12 21:41:45 +0100

    Merge Sort Algorithm with Recursion in Python

--- merge_sort_algo.py
+++ merge_sort_algo.py
@@ -32,5 +32,10 @@ def merge_sort(array):
         right_array_index += 1
         sorted_index += 1
 
+
 if __name__ == '__main__':
-    pass
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
+    print('Sorted array: ' + str(numbers))
