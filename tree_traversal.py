970050f3d913e69f82b009183d93cebf1893e659
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-16 16:25:56 +0100

    Tree Traversal using Binary Search Tree

--- tree_traversal.py
+++ tree_traversal.py
@@ -43,5 +43,34 @@ class BinarySearchTree:
         else:
             if node.left is None:
                 return node.right
+
             elif node.right is None:
-                return node.left                
+                return node.left
+
+            node.key = self._min_value(node.right)
+            node.right = self._delete(node.right, node.key)
+        return node
+
+    def _min_value(self, node):
+        while node.left is not None:
+            node = node.left
+        return node.key
+
+    def inorder_traversal(self):
+        result = []
+        self._inorder_traversal(self.root, result)
+        return result
+
+    def _inorder_traversal(self, node, result):
+        if node:
+            self._inorder_traversal(node.left, result)
+            result.append(node.key)
+            self._inorder_traversal(node.right, result)
+
+bst = BinarySearchTree()
+nodes = [50, 30, 20, 40, 70, 60, 80]
+
+for node in nodes:
+    bst.insert(node)
+
+print("Inorder traversal:", bst.inorder_traversal())
