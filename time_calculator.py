a94a550bc1f0977cbe048b90e56b1aee2b2dd773
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-18 17:29:50 +0100

    Time Calculator in Python

--- time_calculator.py
+++ time_calculator.py
@@ -1,15 +1,5 @@
-# Rules
-# Function takes 3 arguments.
-# 1. A start time in the 12-hour clock format (ending in AM or PM)
-# 2. A duration time that indicates the number of hours and minutes
-# 3. (optional) a starting day of the week, case insensitive
-
-# 1. Do not import any Python libraries.
-# 2. Assume that the start times are valid times.
-# 3. The minutes in the duration time will be a whole number less than 60, but the hour can be any whole number.
-
-testing = '3:12 AM'
-duration2 = '2:32'
+testing = '11:02 AM'
+duration2 = '424:02'
 
 def add_time(start, duration, day=None):
     
@@ -32,9 +22,81 @@ def add_time(start, duration, day=None):
             result = (hour, minutes)
             return result
         
+    # Initializing variables to calculate on
+    weekdays = {'monday': 0, 'tuesday': 1, 'wednesday': 2, 'thursday': 3, 'friday': 4, 'saturday': 5, 'sunday': 6}
+    if day != None:
+        current_day = weekdays.get(day.lower())
+    
+    result_h = 0
+    result_m = 0
+    result_d = 0
+    is_AM = None
+    days_later = 0
+
+    # Extracting start and duration time
     start_tuple = extract_time_to_tuple(start)
     duration_tuple = extract_time_to_tuple(duration)
-
     
+    # Main time calculations
+    if start_tuple[2] == 'AM':
+        is_AM = True
+    elif start_tuple[2] == 'PM':
+        is_AM = False
 
-add_time(testing, duration2)
+    result_h = start_tuple[0] + duration_tuple[0]
+    result_m = start_tuple[1] + duration_tuple[1]
+    
+    while result_m > 60:
+        result_m -= 60
+        result_h += 1
+
+    while result_h > 12:
+        if is_AM == True:
+            is_AM = False 
+        else:
+            is_AM = True
+            days_later += 1
+        result_h -= 12
+
+    if day != None:
+        result_d = ((days_later % 7) + current_day) % 7
+
+    # Output, padding
+    str_result_h = str(result_h)
+    str_result_m = ''
+    str_result_d = ''
+    str_result_ampm = ''
+
+    if result_m < 10:
+        str_result_m = '0' + str(result_m)
+    else:
+        str_result_m = str(result_m)
+
+    for key, val in weekdays.items():
+        if val == result_d:
+            str_result_d = key
+
+    if is_AM == True:
+        str_result_ampm = 'AM'
+    elif is_AM == False:
+        str_result_ampm = 'PM'
+    
+    hour_result = (f'{str_result_h + ':' + str_result_m + ' ' + str_result_ampm}')
+
+    print(days_later)
+    if day == None:
+        if days_later == 0:
+            print(hour_result)
+        elif days_later == 1:
+            print(f'{hour_result} (next day)')
+        elif days_later > 1:
+            print(f'{hour_result} ({days_later} days later)')
+    else:
+        if days_later == 0:
+            print(f'{hour_result}, {str_result_d.capitalize()}')
+        elif days_later == 1:
+            print(f'{hour_result}, {str_result_d.capitalize()} (next day)')
+        elif days_later > 1:
+            print(f'{hour_result}, {str_result_d.capitalize()} ({days_later} days later)')
+
+add_time(testing, duration2, 'Monday')
