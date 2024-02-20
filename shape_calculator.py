8fbd83b1f632db0e82c6fa6da249006c13d18afc
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-20 16:37:51 +0100

    Shape Calculator using OOP in Python

---
+++ shape_calculator.py
@@ -0,0 +1,39 @@
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+    
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return (2 * self.width + 2 * self.height)
+
+
+# Testing
+rect = Rectangle(10, 5)
+print('Area:', rect.get_area())
+print('Perimeter:', rect.get_perimeter())
+rect.set_height(3)
+print('Area:', rect.get_area())
+print('Perimeter:', rect.get_perimeter())
+# print(rect)
+# print(rect.get_picture())
+
+# sq = Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
+# print(sq.get_picture())
+
+# rect.set_height(8)
+# rect.set_width(16)
+# print(rect.get_amount_inside(sq))
