d34ce64e0c54d5c356a43df79f8eaf2c89928a18
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-02-21 19:03:19 +0100

    Probability Calculator using OOP with random, kwargs, deepcopy, dict approach

--- probability_calculator_list.py
+++ probability_calculator_list.py
@@ -8,6 +8,8 @@ class Hat:
         if not kwargs:
             raise ValueError("You need to pass at least one argument to the function.")
         
+        print(kwargs)
+
         # Initialize two lists for operations below
         self.unpack = []
         self.contents = []
@@ -36,19 +38,32 @@ class Hat:
             
         for word in draw_result:
             result_dict[word] = result_dict.get(word, 0) + 1
-
+        
         return result_dict
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     experiment_draw = []
-
+    count = 0
+    experiment_success_count = 0
+    
     for i in range(num_experiments):
+        count = 0
         hat_reset = copy.deepcopy(hat)
         experiment_draw = hat_reset.draw(num_balls_drawn)
-        print('Results:', experiment_draw)
+        
+        for key, val in expected_balls.items():
+            try:
+                if expected_balls[key] <= experiment_draw[key]:
+                    count += 1
+            except:
+                continue        
+        if count == len(expected_balls):
+            experiment_success_count += 1
 
-test_dict = {"yellow":2, "green":2}
+    probability = experiment_success_count / (num_experiments - 1) * 100
+    return probability
 
+
+test_dict={"blue":2,"green":1}
 hat = Hat(yellow=10, green=10, blue=10)
-
-experiment(hat, test_dict, 5, 5)
+print(experiment(hat, test_dict, 4, 1000))
